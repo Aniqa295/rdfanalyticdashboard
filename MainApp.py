0c841485d254beb77e38194a8cd30a8ac70b9ef8
Aniqa <AniqaRiaz29@gmail.com>
2021-02-21 21:26:36 +0100

    styles

--- MainApp.py
+++ MainApp.py
@@ -3,6 +3,6 @@ import dash
 
 
 # Build App
-external_stylesheets = ['https://codepen.io/ne9ar/pen/gOLGrwv.css']
+external_stylesheets = ['http://jsfiddle.net/ne9ar/4ny7s0ou.css']
 app = dash.Dash(__name__,external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)
 
