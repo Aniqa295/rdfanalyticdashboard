2f2beee8814475245b9633b696c7f9229f85e75f
Aniqa <AniqaRiaz29@gmail.com>
2021-02-21 21:06:30 +0100

    styles

--- MainApp.py
+++ MainApp.py
@@ -1,7 +1,9 @@
 import dash
-
-
+import dash_core_components as dcc
 
 
 # Build App
+dcc._css_dist[0][‘relative_package_path’].append(‘styles.css’)
 app = dash.Dash(__name__,suppress_callback_exceptions=True)
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
