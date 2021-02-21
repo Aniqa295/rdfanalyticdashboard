95699afa51b9cb636398d428ad281f094c026741
Aniqa <AniqaRiaz29@gmail.com>
2021-02-21 21:12:12 +0100

    styles sheet

--- MainApp.py
+++ MainApp.py
@@ -1,9 +1,8 @@
 import dash
-import dash_core_components as dcc
+
 
 
 # Build App
-dcc._css_dist[0][‘relative_package_path’].append(‘styles.css’)
-app = dash.Dash(__name__,suppress_callback_exceptions=True)
-app.css.config.serve_locally = True
-app.scripts.config.serve_locally = True
+external_stylesheets = ['https://codepen.io/ne9ar/pen/gOLGrwv.css']
+app = dash.Dash(__name__,external_stylesheets=external_stylesheets,suppress_callback_exceptions=True)
+
